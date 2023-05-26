4407225c1d55115b1567c1ed37f4bdc6bd652787
Eva Useros <79694729+Evusma@users.noreply.github.com>
2023-05-26 15:48:11 +0200

    Add files via upload

---
+++ trigger_function_pgmetadata_date.sql
@@ -0,0 +1,17 @@
+-- Trigger function to update the date of last modification of the data in the metadata table (metadata managed by the QGIS plugin pgmetadata);
+
+CREATE FUNCTION __donnees.pgmetadata_date_p()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+    IF (TG_OP IN ('INSERT', 'UPDATE', 'DELETE')) THEN
+	UPDATE pgmetadata.dataset
+	   SET data_last_update = now() 
+	 WHERE table_name = TG_TABLE_NAME;
+    	RETURN NEW;
+    END IF;
+END;
+$BODY$;
