e66f6c03d124836f63275037918aaf8d3ced0a25
Eva Useros <79694729+Evusma@users.noreply.github.com>
2023-05-30 12:28:08 +0200

    Update trigger_function_history_espaces_verts.sql

--- trigger_function_history_espaces_verts.sql
+++ trigger_function_history_espaces_verts.sql
@@ -11,9 +11,9 @@ BEGIN
 	IF (TG_OP = 'DELETE') THEN
 
 		INSERT INTO __historique.history_nsm_gestion_espaces_verts (id, geom, parcelle, entite, categorie, etiquette, nom, contenant, 
-																	designation, secteur, id_secteur, aspect, observation, deleted_by, maj_deleted)
+									    designation, secteur, id_secteur, aspect, observation, deleted_by, maj_deleted)
 		VALUES (OLD.id, OLD.geom, OLD.parcelle, OLD.entite, OLD.categorie, OLD.etiquette, OLD.nom, OLD.contenant, 
-				OLD.designation, OLD.secteur, OLD.id_secteur, OLD.aspect, OLD.observation, current_user, tstzrange(current_timestamp, NULL));
+			OLD.designation, OLD.secteur, OLD.id_secteur, OLD.aspect, OLD.observation, current_user, tstzrange(current_timestamp, NULL));
 		RETURN OLD;
 		
 	END IF;
@@ -21,7 +21,7 @@ BEGIN
 	IF (TG_OP = 'INSERT') THEN
 	
 		INSERT INTO __historique.history_nsm_gestion_espaces_verts (id, geom, parcelle, entite, categorie, etiquette, nom, contenant, 
-																	designation, secteur, id_secteur, aspect, observation, created_by, maj_created)
+									    designation, secteur, id_secteur, aspect, observation, created_by, maj_created)
 		VALUES (NEW.id, NEW.geom, NEW.parcelle, NEW.entite, NEW.categorie, NEW.etiquette, NEW.nom, NEW.contenant, 
 		    	NEW.designation, NEW.secteur, NEW.id_secteur, NEW.aspect, NEW.observation, current_user, tstzrange(current_timestamp, NULL));
 		RETURN NEW;
@@ -30,14 +30,14 @@ BEGIN
 	IF (TG_OP = 'UPDATE') THEN
 		
 		INSERT INTO __historique.history_nsm_gestion_espaces_verts (id, geom, parcelle, entite, categorie, etiquette, nom, contenant, 
-																	designation, secteur, id_secteur, aspect, observation)
+									    designation, secteur, id_secteur, aspect, observation)
 		VALUES (OLD.id, OLD.geom, OLD.parcelle, OLD.entite, OLD.categorie, OLD.etiquette, OLD.nom, OLD.contenant, 
-				OLD.designation, OLD.secteur, OLD.id_secteur, OLD.aspect,' (avant la mise à jour)');
+			OLD.designation, OLD.secteur, OLD.id_secteur, OLD.aspect,' (avant la mise à jour)');
 			
 		INSERT INTO __historique.history_nsm_gestion_espaces_verts (id, geom, parcelle, entite, categorie, etiquette, nom, contenant, 
-																	designation, secteur, id_secteur, aspect, observation, updated_by, maj_updated)
+									    designation, secteur, id_secteur, aspect, observation, updated_by, maj_updated)
 		VALUES (NEW.id, NEW.geom, NEW.parcelle, NEW.entite, NEW.categorie, NEW.etiquette, NEW.nom, NEW.contenant, 
-				NEW.designation, NEW.secteur, NEW.id_secteur, NEW.aspect, NEW.observation||' (après la mise à jour)', current_user, tstzrange(current_timestamp, NULL));
+			NEW.designation, NEW.secteur, NEW.id_secteur, NEW.aspect, NEW.observation||' (après la mise à jour)', current_user, tstzrange(current_timestamp, NULL));
 		RETURN NEW;	
 	END IF;
 
