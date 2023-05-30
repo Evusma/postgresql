b1c77c189f1016dc802a0d1d9995e74b7237b37a
Eva Useros <79694729+Evusma@users.noreply.github.com>
2023-05-30 11:32:52 +0200

    Add files via upload

---
+++ view_add_surface.sql
@@ -0,0 +1,7 @@
+-- this view uses the postgis function st_area() to add the surface (m²) of the buildings
+
+CREATE OR REPLACE VIEW referentiel.surface_bati AS
+	SELECT  "BATIMENT"."GEOM",
+			"BATIMENT"."ID",
+			st_area("BATIMENT"."GEOM") AS m2_bati
+    FROM 	edigeo."BATIMENT";
